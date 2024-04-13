dd012e6c87a2290b6f61526c482b0ea894465a93
Aryan Saini <sainiaryan2222@gmail.com>
2024-04-13 17:08:19 +0530

    branching test 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Home() {
-  return <div>THIS IS HOME FROM DEV Branch 2</div>;
+  return (
+    <div>
+      This is aryan Branch and I want to see these changes on Development branch
+    </div>
+  );
 }
 
 export default Home;
