7ecfdd4aa7ae9d1d052db56ff358e5ff4f481fc6
Tushar Aggaqrwal <aggtushar123@gmail.com>
2024-04-13 17:33:29 +0530

    Tushar Changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Home() {
-  return <div>this is aryansaini branch test </div>;
+  return <div>this is tushar dsadssad branch test </div>;
 }
 
 export default Home;
