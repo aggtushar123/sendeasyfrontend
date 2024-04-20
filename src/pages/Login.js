f4a36a1c313f89c6bf21ed2b23c77543845810c2
Aryan Saini <sainiaryan2222@gmail.com>
2024-04-20 17:48:31 +0530

    forgot password modal added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,28 +1,29 @@
-import * as React from "react";
-import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import EmailIcon from "../components/assets/Login/EmailIcon.svg";
-import PasswordIcon from "../components/assets/Login/PasswordIcon.svg";
-import "react-dropdown/style.css";
-import { useSelector, useDispatch } from "react-redux";
-import { toggleFalse, toggleTrue } from "../features/headerSlice";
-import logo from "../components/assets/Group.png";
-import image from "../components/assets/Login/main.svg";
-import GoogleIcon from "../components/assets/Login/GoogleIcon.svg";
-import FacebookIcon from "../components/assets/Login/FaebookIcon.svg";
-import Dropdown from "../components/Dropdown";
-import { login } from "../features/auth/authSlice";
+import * as React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import EmailIcon from '../components/assets/Login/EmailIcon.svg';
+import PasswordIcon from '../components/assets/Login/PasswordIcon.svg';
+import 'react-dropdown/style.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleFalse, toggleTrue } from '../features/headerSlice';
+import logo from '../components/assets/Group.png';
+import image from '../components/assets/Login/main.svg';
+import GoogleIcon from '../components/assets/Login/GoogleIcon.svg';
+import FacebookIcon from '../components/assets/Login/FaebookIcon.svg';
+import Dropdown from '../components/Dropdown';
+import { login } from '../features/auth/authSlice';
 
 const Login = () => {
   const dispatch = useDispatch();
   const [showPassword, setShowPassword] = useState(false);
-  const options = ["Traveler Listing", "Luggage Listing", "Create a Listing"];
+  const [showModal, setShowModal] = useState(false);
+  const options = ['Traveler Listing', 'Luggage Listing', 'Create a Listing'];
   const [selectedOption, setSelectedOption] = useState();
   const navigate = useNavigate();
   const [isOpen, setIsOpen] = useState(false);
   const [loginData, setLoginData] = useState({
-    email: "",
-    password: "",
+    email: '',
+    password: '',
   });
   const { email, password } = loginData;
 
@@ -35,7 +36,7 @@ const Login = () => {
   };
   const handleDropdownChange = (selected) => {
     setSelectedOption(selected.value);
-    navigate(`/${selected.value.toLowerCase().replace(/\s/g, "")}`);
+    navigate(`/${selected.value.toLowerCase().replace(/\s/g, '')}`);
   };
   const handleLoginToggle = () => {
     dispatch(toggleFalse());
@@ -61,109 +62,175 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col pt-5 pr-4 pb-20 pl-15 rounded-[29px] max-md:pl-5">
-      <div className="flex gap-2 justify-between self-center mt-7 w-full max-w-[1080px] max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-        <img
-          loading="lazy"
-          src={image}
-          className="self-start w-full aspect-[1.1] max-md:max-w-full"
-        />
-        <div className="flex flex-col mt-3.5 text-sm tracking-wide text-slate-400">
-          <div className="text-3xl font-medium tracking-wider leading-10 text-sky-400 max-md:ml-1.5">
-            <span className="">Let’s</span>{" "}
-            <span className="font-extrabold text-sky-400">Sign In</span>{" "}
-          </div>
-          <div className="mt-1 whitespace-nowrap leading-[143%] text-slate-600 max-md:ml-1.5">
-            quis nostrud exercitation ullamco laboris nisi ut
-          </div>
-          <form
-            className="flex flex-col text-sm mt-3 tracking-wide text-slate-400"
-            onSubmit={onSubmit}
-          >
-            <div className="relative mt-2">
-              <input
-                type="email"
-                className="pl-10 pr-4 py-2 border rounded-xl bg-gray-100 w-[327px] h-[70px] "
-                placeholder="Email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={onChange}
-                required
-              />
-              <div
-                className="absolute inset-y-0 left-0 pl-3  
-                    flex items-center  
-                    pointer-events-none"
-              >
-                <img src={EmailIcon} alt="" />
+    <>
+      {showModal && (
+        <div className='flex justify-center items-center'>
+          <div className='fixed inset-0 z-10 w-screen overflow-y-auto'>
+            <div className='flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0'>
+              {' '}
+              <div className='fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity'></div>
+              <div className='relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg'>
+                <div className='bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4'>
+                  <div className='sm:flex sm:items-start'>
+                    <div className='mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left'>
+                      <h3
+                        className='text-base font-semibold leading-6 text-gray-900'
+                        id='modal-title'
+                      >
+                        <div className='self-start mt-1 ml-5 text-2xl font-medium tracking-wider leading-10 text-sky-400'>
+                          Search your
+                          <span className='font-extrabold text-sky-400'>
+                            {' '}
+                            Id
+                          </span>
+                        </div>
+                        <div className='mt-5 ml-5 text-xs tracking-wide leading-5 text-slate-600 '>
+                          Put your email or mobile number to find your id
+                        </div>
+                      </h3>
+                      <div class='relative mt-5'>
+                        <input
+                          type='email'
+                          class='pl-10 pr-4 py-2 border rounded-xl bg-gray-100 w-[327px] h-[70px] '
+                          placeholder='Email'
+                        />
+                        <div
+                          class='absolute inset-y-0 left-0 pl-3  
+                      flex items-center  
+                      pointer-events-none'
+                        >
+                          <img src={EmailIcon} alt='' />
+                        </div>
+                      </div>
+
+                      <button
+                        className='justify-center items-center self-center px-16 py-7 mt-10 max-w-full text-base font-bold tracking-wide text-center text-white whitespace-nowrap bg-sky-400 rounded-[29px] w-[278px]'
+                        onClick={() => {
+                          setShowModal(false);
+                        }}
+                      >
+                        Next
+                      </button>
+                    </div>
+                  </div>
+                </div>
+                <div className='bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6'></div>
               </div>
             </div>
-            <div className="relative mt-2">
-              <input
-                type={showPassword ? "text" : "password"}
-                className="pl-10 pr-4 py-2 border rounded-xl bg-gray-100 w-[327px] h-[70px] "
-                placeholder="Password"
-                id="password"
-                name="password"
-                value={password}
-                onChange={onChange}
-                required
-              />
-              <div
-                className="absolute inset-y-0 left-0 pl-3  
-                    flex items-center  
-                    pointer-events-none"
-              >
-                <img src={PasswordIcon} alt="" />
-              </div>
+          </div>
+        </div>
+      )}
+
+      <div className='flex flex-col pt-5 pr-4 pb-20 pl-15 rounded-[29px] max-md:pl-5'>
+        <div className='flex gap-2 justify-between self-center mt-7 w-full max-w-[1080px] max-md:flex-wrap max-md:mt-10 max-md:max-w-full'>
+          <img
+            loading='lazy'
+            src={image}
+            className='self-start w-full aspect-[1.1] max-md:max-w-full'
+          />
+          <div className='flex flex-col mt-3.5 text-sm tracking-wide text-slate-400'>
+            <div className='text-3xl font-medium tracking-wider leading-10 text-sky-400 max-md:ml-1.5'>
+              <span className=''>Let’s</span>{' '}
+              <span className='font-extrabold text-sky-400'>Sign In</span>{' '}
             </div>
-            <div className="flex gap-5 justify-between mt-4 font-semibold text-sky-400 leading-[143%] max-md:mr-2">
-              <button 
-              type="button"
-              className="flex-auto">Forgot password?</button>
-              <button
-                type="button"
-                className="flex-auto text-right"
-                onClick={handleShowPassword}
-              >
-                {showPassword ? "Hide" : "Show"} password
+            <div className='mt-1 whitespace-nowrap leading-[143%] text-slate-600 max-md:ml-1.5'>
+              quis nostrud exercitation ullamco laboris nisi ut
+            </div>
+            <form
+              className='flex flex-col text-sm mt-3 tracking-wide text-slate-400'
+              onSubmit={onSubmit}
+            >
+              <div className='relative mt-2'>
+                <input
+                  type='email'
+                  className='pl-10 pr-4 py-2 border rounded-xl bg-gray-100 w-[327px] h-[70px] '
+                  placeholder='Email'
+                  id='email'
+                  name='email'
+                  value={email}
+                  onChange={onChange}
+                  required
+                />
+                <div
+                  className='absolute inset-y-0 left-0 pl-3  
+                    flex items-center  
+                    pointer-events-none'
+                >
+                  <img src={EmailIcon} alt='' />
+                </div>
+              </div>
+              <div className='relative mt-2'>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  className='pl-10 pr-4 py-2 border rounded-xl bg-gray-100 w-[327px] h-[70px] '
+                  placeholder='Password'
+                  id='password'
+                  name='password'
+                  value={password}
+                  onChange={onChange}
+                  required
+                />
+                <div
+                  className='absolute inset-y-0 left-0 pl-3  
+                    flex items-center  
+                    pointer-events-none'
+                >
+                  <img src={PasswordIcon} alt='' />
+                </div>
+              </div>
+              <div className='flex gap-5 justify-between mt-4 font-semibold text-sky-400 leading-[143%] max-md:mr-2'>
+                <button
+                  type='button'
+                  className='flex-auto'
+                  onClick={() => {
+                    setShowModal(true);
+                  }}
+                >
+                  Forgot password?
+                </button>
+                <button
+                  type='button'
+                  className='flex-auto text-right'
+                  onClick={handleShowPassword}
+                >
+                  {showPassword ? 'Hide' : 'Show'} password
+                </button>
+              </div>
+              <button className='justify-between items-center self-center px-16 py-6 mt-6 max-w-full text-base font-bold tracking-wide text-center text-white whitespace-nowrap bg-sky-400 rounded-[29px] w-[278px] max-md:px-5'>
+                Login
+              </button>
+            </form>
+
+            <div className='flex justify-center items-center px-16 mt-3 font-semibold text-center whitespace-nowrap max-md:px-5 max-md:mr-2'>
+              <div className='justify-center px-2.5 py-1.5 bg-white'>OR</div>
+            </div>
+
+            <div className='flex gap-3 mt-4'>
+              <button className='flex flex-1 justify-center items-center px-16 py-6 bg-gray-100 rounded-3xl max-md:px-5'>
+                <img
+                  loading='lazy'
+                  src={GoogleIcon}
+                  className='aspect-square w-[25px]'
+                />
+              </button>
+              <button className='flex flex-1 justify-center items-center px-16 py-6 bg-gray-100 rounded-3xl max-md:px-5'>
+                <img
+                  loading='lazy'
+                  src={FacebookIcon}
+                  className='aspect-square w-[25px]'
+                />
               </button>
             </div>
-            <button className="justify-between items-center self-center px-16 py-6 mt-6 max-w-full text-base font-bold tracking-wide text-center text-white whitespace-nowrap bg-sky-400 rounded-[29px] w-[278px] max-md:px-5">
-              Login
-            </button>
-          </form>
-
-          <div className="flex justify-center items-center px-16 mt-3 font-semibold text-center whitespace-nowrap max-md:px-5 max-md:mr-2">
-            <div className="justify-center px-2.5 py-1.5 bg-white">OR</div>
-          </div>
-
-          <div className="flex gap-3 mt-4">
-            <button className="flex flex-1 justify-center items-center px-16 py-6 bg-gray-100 rounded-3xl max-md:px-5">
-              <img
-                loading="lazy"
-                src={GoogleIcon}
-                className="aspect-square w-[25px]"
-              />
-            </button>
-            <button className="flex flex-1 justify-center items-center px-16 py-6 bg-gray-100 rounded-3xl max-md:px-5">
-              <img
-                loading="lazy"
-                src={FacebookIcon}
-                className="aspect-square w-[25px]"
-              />
-            </button>
-          </div>
-          <div className="self-center mt-20 text-sm tracking-wide leading-5 text-cyan-900 whitespace-nowrap max-md:mt-10">
-            <span className="">Don’t have an account? </span>
-            <Link className="font-bold text-cyan-900" to="/signup">
-              Sign Up
-            </Link>
+            <div className='self-center mt-20 text-sm tracking-wide leading-5 text-cyan-900 whitespace-nowrap max-md:mt-10'>
+              <span className=''>Don’t have an account? </span>
+              <Link className='font-bold text-cyan-900' to='/signup'>
+                Sign Up
+              </Link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
