4735139c2e07909398b0d8e37d4d3aad5218708f
Tushar Aggaqrwal <aggtushar123@gmail.com>
2024-04-24 14:49:42 +0530

    Send Otp Fixed

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { useSelector, useDispatch } from "react-redux";
 
 const baseURL = 'http://localhost:5000'; // Change the port to 5000 or your desired backend port
 const API_URL = `${baseURL}/api/users/`;
@@ -14,42 +13,43 @@ const axiosInstance = axios.create({
 const register = async (userData) => {
   const response = await axiosInstance.post(`${API_URL}signup`, userData);
     console.log(response);
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data));
-  }
-
-  setTimeout(async () => {
-
-      const userResponse = await axiosInstance.get(`${API_URL}user/${response.data.data.userId}`);
-      console.log(userResponse);
-      if (userResponse.data.exists === false) {
-        localStorage.removeItem('user');
-      } 
-      else{
-        localStorage.setItem('user',JSON.stringify(userResponse.data.user) )
-      } 
-
-  }, 26000);
   return response.data;
 };
 
 // Login
 const login = async (userData) => {
   const response = await axiosInstance.post(`${API_URL}login`, userData);
-    // console.log("login wala response",response)
+
   if (response.data) {
     localStorage.setItem('user', JSON.stringify(response.data));
   }
   return response.data;
 };
 
+const sendOtp = async (otpData) => {
+  try {
+    const response = await axiosInstance.post(`${API_URL}sendOtp`, otpData);
+  console.log(response);
+  console.log(response.data.user);
+  if (response.data) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+    
+  }
+    return response
+  } catch (error) {
+    throw new Error(error.response?.data?.message || error.message || 'Error sending OTP');
+  }
+};
 // Logout User
 const logout = () => localStorage.removeItem('user');
+
+
 
 const authService = {
   register,
   logout,
   login,
+  sendOtp
 };
 
 export default authService;
